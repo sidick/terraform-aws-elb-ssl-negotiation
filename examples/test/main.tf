41f68edaeac398e0d405b07eb705096f6d80cf15
Simon Dick <simond@irrelevant.org>
2017-12-19 14:27:35 +0000

    Update example and fix order of a variables file

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 module "ssl_policy" {
-  source = "../.."
+  source = "../../modules/ELBSecurityPolicy-TLS-1-2-2017-01"
 
-  ssl_policy_name = "foo-policy"
-  load_balancer_id = "simontest-lb"
+  ssl_policy_name    = "foo-policy"
+  load_balancer_id   = "simontest-lb"
   load_balancer_port = "443"
 }
