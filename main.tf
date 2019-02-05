d2bc6ba693f9bb16bbd38113681f16cecd066332
amit.gera <amit.gera@oyorooms.com>
2019-02-05 15:25:03 +0530

    tf-module: fixing bug

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # This file is auto generated, please do not edit by hand
 
 resource "aws_lb_ssl_negotiation_policy" "policy" {
-  count         = "${var.create}"
+  count         = "${var.create ? 1 : 0}"
   name          = "${var.ssl_policy_name}"
   load_balancer = "${var.load_balancer_id}"
   lb_port       = "${var.load_balancer_port}"
